68193cd7244fa557bbc51d25f111a0a60b7bbdf0
Nina Buie <ninarachaebuie@gmail.com>
2021-04-06 13:54:20 -0500

    added pete mayor's header code

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ let messageid = 2;
 
 app.use(express.json());
 
-//need the app.use header starter code
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World');

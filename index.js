5c8245df70b6a36a7d8ea98cbad6de130be17934
Nina Buie <ninarachaebuie@gmail.com>
2021-04-23 17:23:04 -0500

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = require("socket.io")(server, {
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-//app.use(cors());
+app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 app.use(function (req, res, next) {
